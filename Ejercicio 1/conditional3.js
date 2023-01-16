ab07a370a7785b9281ced40f742baa754569766e
KarlaLilibeth <122765143+KarlaLilibeth@users.noreply.github.com>
2023-01-15 22:42:24 -0500

    Tarea
    Tarea en clase con la utilización de JavaScript

---
+++ Ejercicio 1/conditional3.js
@@ -0,0 +1,10 @@
+let age=prompt("ingrese la edad");
+const AGE_MAX1=5;
+const AGE_MIN=65;
+if(age<AGE_MAX1||age>AGE_MIN){
+    console.log("Puede acceder a la vacuna");
+}
+else{
+    console.log("Lo siento, no puede acceder a la vacuna");
+}
+console.log("Adios");
